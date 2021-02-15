9534dd7cbbe513b1603276eb45c0c3c2650b9994
eder <eder@localhost.localdomain>
2021-02-15 23:38:35 +0100

    sobre nosotros

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 const typed = new Typed('.typed', {
 	strings: [
 		'<p class="mascota">"No vendemos propiedades, cumplimos sueños"</p>',
-		'<p class="nada">  </p>',
     '<p class="mascota">"Nuestra prioridad es tu satisfacción"</p>',
 	],
 
 	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
 	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
 	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-	backSpeed: 30, // Velocidad en milisegundos para borrrar una letra,
+	backSpeed: 0, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
 	backDelay: 7000, // Tiempo de espera despues de que termina de escribir una palabra.
